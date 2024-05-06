03bcd6f30b0421e2d6f10e4333f79d7af6e8e881
Leonardo Soares <leonardofs88@live.com>
2024-05-06 22:19:39 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Leonardo Soares on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
